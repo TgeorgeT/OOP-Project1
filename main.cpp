04f4e6b7cdf9f1f057980ac2d90b30980538c050
TgeorgeT <76885076+TgeorgeT@users.noreply.github.com>
2022-03-22 21:01:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -39,22 +41,24 @@ public:
         return val;
     }
 
+    Nod operator=(Nod);
+
 };
+
+Nod Nod::operator=(Nod a) {
+    n=a.n;
+    p=a.p;
+    val=a.val;
+    return *this;
+}
 
 class Lista {
     Nod *start, *final;
     int n;
 
-    Nod *operator[](int poz) {
-        if (poz > n)
-            return nullptr;
-        Nod *aux;
-        for (aux = start; poz > 0 && aux != nullptr; poz--)
-            aux = aux->next();
-        return aux;
-    }
 
 public:
+
     Lista() {
         start = final = nullptr;
         n = 0;
@@ -66,6 +70,11 @@ public:
     }
 
     Lista(const Lista &l) {
+        if (l.n == 0) {
+            final = start = NULL;
+            n = 0;
+            return;
+        }
         final = start = new Nod(l.start->getValue());
         n = 1;
         for (Nod *aux = l.start->next(); aux != nullptr; aux = aux->next()) {
@@ -100,6 +109,8 @@ public:
         return n;
     }
 
+    Nod *operator[](int poz);
+
     friend ostream &operator<<(ostream &, Lista);
 
     friend istream &operator>>(istream &, Lista &);
@@ -120,17 +131,33 @@ public:
 
     void push_front(int);
 
-    void pop_front();
+    int pop_front();
 
-    void pop_back();
+    int pop_back();
 };
 
-void Lista::pop_back() {
-    eliminare(n - 1);
+Nod *Lista::operator[](int poz) {
+
+    if (poz > n)
+        return nullptr;
+    Nod *aux;
+    for (aux = start; poz > 0 && aux != nullptr; poz--)
+        aux = aux->next();
+    return aux;
 }
 
-void Lista::pop_front() {
+int Lista::pop_back() {
+    if (n == 0)
+        return -1;
+    eliminare(n - 1);
+    return 0;
+}
+
+int Lista::pop_front() {
+    if (n == 0)
+        return -1;
     eliminare(0);
+    return 0;
 
 }
 
@@ -142,16 +169,24 @@ void Lista::eliminare(int poz) {
         Nod *aux = start;
         start = start->next();
         delete aux;
-        start->setPrev(nullptr);
         n--;
+        if (start == nullptr) {
+            final = start;
+            return;
+        }
+        start->setPrev(nullptr);
         return;
     }
     if (poz == n - 1) {
         Nod *aux = final;
         final = final->prev();
         delete aux;
-        final->setNext(nullptr);
         n--;
+        if (final == nullptr) {
+            start = final;
+            return;
+        }
+        final->setNext(nullptr);
         return;
     }
     Nod *aux = (*this)[poz];
@@ -207,6 +242,8 @@ Lista operator+(int val, Lista a) {
 }
 
 ostream &operator<<(ostream &out, Lista a) {
+    if (a.n == 0)
+        return out;
     for (Nod *aux = a.start; aux != nullptr; aux = aux->next())
         out << aux->getValue() << " ";
     return out;
@@ -265,12 +302,12 @@ public:
         d.push_front(val);
     }
 
-    void pop_back() {
-        d.pop_back();
+    int pop_back() {
+        return d.pop_back();
     }
 
-    void pop_front() {
-        d.pop_front();
+    int pop_front() {
+        return d.pop_front();
     }
 
     int front() {
@@ -278,7 +315,7 @@ public:
     }
 
     int back() {
-        return d.interogarePoz(d.getNr());
+        return d.interogarePoz(d.getNr() - 1);
     }
 
     friend ostream &operator<<(ostream &, Dequeue);
@@ -287,13 +324,24 @@ public:
 
     Dequeue operator+(int);
 
+    void operator+=(int val) {
+        d = d + val;
+    }
+
     friend Dequeue operator+(int, Dequeue);
 
     operator int() {
         return this->front();
     }
 
+    Nod *operator[](int);
+
 };
+
+Nod *Dequeue::operator[](int poz) {
+    return d[poz];
+
+}
 
 ostream &operator<<(ostream &out, Dequeue a) {
     cout << a.d;
@@ -317,14 +365,188 @@ Dequeue Dequeue::operator+(int val) {
     return sum;
 }
 
+inline void rtrim(string &s) {
+    s.erase(s.find_last_not_of(' ') + 1);
+}
+
+inline void ltrim(string &s) {
+    s.erase(0, s.find_first_not_of(' '));
+}
+
+inline void trim(string &s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+int isnum(string a)
+{
+    for(int i=0;i<a.length();i++)
+        if(!(a[i]>='0' && a[i]<='9'))
+            return -1;
+    return 0;
+}
+
 int main() {
-    Dequeue a, d(2);
-    a = d + 1;
-    a = 1 + a;
-    a.push_back(5);
-    a.pop_front();
-    a.push_front(10);
-    int b=a;
-    cout<<b;
+    string ans;
+    trim(ans);
+    cout << "Do you want to create a new dequeue?(y/n)\n";
+    while (1) {
+        getline(cin, ans);
+        trim(ans);
+        if (ans.length() != 1 || strchr("ny",ans[0])==NULL)
+            cout << "Please type (y/n)\n";
+        else {
+            if (tolower(ans[0]) == 'n')
+                exit(0);
+            break;
+        }
+    }
+    cout << "#########################################\n\n";
+    cout << "Insert the first number into the dequeue:\n";
+    int nr;
+    while (1) {
+        string number;
+        getline(cin, number);
+        trim(number);
+        try {
+            if(isnum(number)==-1)
+                throw invalid_argument("");
+            nr = stoi(number);
+        }
+        catch (const exception &e) {
+            cout << "Invalid number\n";
+            continue;
+        }
+
+        break;
+    }
+    Dequeue d(nr);
+    while (1) {
+        cout << "\n#########################################\n\n";
+        cout << "Your dequeue:\n" << d << "\n\n";
+        cout << "Pick an option:\n";
+        cout << "0. Exit\n";
+        cout << "1. Push back a number\n";
+        cout << "2. Push front a number\n";
+        cout << "3. Pop back\n";
+        cout << "4. Pop front\n";
+        cout << "5. Get the front element\n";
+        cout << "6. Get the back element\n";
+        cout << "7. Get the element at the n-th position\n";
+        getline(cin, ans);
+        int option = -1;
+        try {
+            option = stoi(ans);
+            if (option > 7)
+                throw invalid_argument("");
+        }
+        catch (const exception &e) {
+            cout << "Please type a number from 0 to 4\n";
+            continue;
+        }
+        if (option == 0) {
+            exit(1);
+        }
+        if (option == 1) {
+            while (1) {
+
+                cout << "\n#########################################\n\n";
+                cout << "Type b to go back or type a number:\n";
+                string number;
+                int nr;
+                getline(cin, number);
+                trim(number);
+                if (number == "b")
+                    break;
+                try {
+                    if(isnum(number)==-1)
+                        throw invalid_argument("");
+                    nr = stoi(number);
+                }
+                catch (const exception &e) {
+                    cout << "Invalid input\n";
+                    continue;
+                }
+                d.push_back(nr);
+                break;
+            }
+        } else if (option == 2) {
+            while (1) {
+
+                cout << "\n#########################################\n\n";
+                cout << "Type b to go back or type a number:\n";
+                string number;
+                int nr;
+                getline(cin, number);
+                trim(number);
+                if (number == "b")
+                    break;
+                try {
+                    if(isnum(number)==-1)
+                        throw invalid_argument("");
+                    nr = stoi(number);
+                }
+                catch (const exception &e) {
+                    cout << "Invalid input\n";
+                    continue;
+                }
+                d.push_front(nr);
+                break;
+            }
+
+        } else if (option == 3) {
+            try {
+                if (d.pop_back())
+                    throw 1;
+            }
+            catch (int x) {
+                cout << "\nDequeue is empty\n";
+            }
+        } else if (option == 4) {
+            try {
+                if (d.pop_front())
+                    throw 1;
+            }
+            catch (int x) {
+                cout << "\nDequeue is empty\n";
+            }
+        } else if (option == 5) {
+            cout << (int) d << '\n';
+        } else if (option == 6) {
+            cout << d.back() << '\n';
+        } else if (option == 7) {
+            while (1) {
+                cout << "\n#########################################\n\n";
+                cout << "Type b to go back or type a position:\n";
+                string number;
+                int nr;
+                getline(cin, number);
+                trim(number);
+                if (number == "b")
+                    break;
+                try {
+                    if(isnum(number)==-1)
+                        throw invalid_argument("");
+                    nr = stoi(number);
+                }
+                catch (const exception &e) {
+                    cout << "Invalid input\n";
+                    continue;
+                }
+                Nod *aux;
+                try {
+                    aux = d[nr];
+                    if (aux == nullptr)
+                        throw 1;
+                }
+                catch (int val) {
+                    cout << "Position out of range\n";
+                    continue;
+                }
+                cout << aux->getValue()<<"\n";
+                break;
+            }
+        }
+    }
     return 0;
 }
